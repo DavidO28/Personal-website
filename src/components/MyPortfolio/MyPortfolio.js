455efb094fe8db7a806c2a3f28b8db52a2cf0a52
DavidO28 <damers22@yahoo.com>
2023-09-16 12:48:21 +0400

    fixes and new features

--- src/components/MyPortfolio/MyPortfolio.js
+++ src/components/MyPortfolio/MyPortfolio.js
@@ -1,8 +1,7 @@
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/scss';
-import 'swiper/scss/navigation';
-import 'swiper/scss/pagination';
+import "./myportfolio.scss";
 import AmindiPic from "../../assets/amindiSLIDER.png";
+import TodolistPic from "../../assets/todolisti.png";
+import QuizPic from "../../assets/quiz.png";
 
 export default function MyPortfolio() {
 
@@ -12,32 +11,16 @@ export default function MyPortfolio() {
         <div className="myportfolio" id="Myportfolio">
             <h2>My portfolio</h2>
             <h3>Here you can see all the projects I have made, apart from small exercises</h3>
-            <div className="porfolioItems">
-                <Swiper
-                    spaceBetween={10}
-                    slidesPerView={2}
-                    onSwiper={(swiper) => console.log(swiper)}
-                >
-                    <SwiperSlide>
-
-                        <a target="_blank" href="https://davido28.github.io/weather-app/">
-                            <img className='sliderIMG' src={AmindiPic} alt="amindi" />
-                            amindi
-                        </a>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <a target="_blank" href="https://davido28.github.io/weather-app/">
-                            <img className='sliderIMG' src={AmindiPic} alt="amindi" />
-                            amindi2
-                        </a>
-                    </SwiperSlide>
-                    <SwiperSlide className='lameri'>
-                        <a target="_blank" href="https://davido28.github.io/weather-app/">
-                            <img className='sliderIMG' src={AmindiPic} alt="amindi" />
-                            amindi3
-                        </a>
-                    </SwiperSlide>
-                </Swiper>
+            <div className="portfolioSlider">
+                <a className="amindi" href="https://davido28.github.io/weather-app/" target="_blank" >
+                    <img src={AmindiPic} alt="amindi" />
+                </a>
+                <a className="todolisti" href="https://davido28.github.io/to-do-list/" target="_blank" >
+                    <img src={TodolistPic} alt="todolist" />
+                </a>
+                <a className="quizi" href="https://davido28.github.io/millionaire-quiz/" target="_blank" >
+                    <img src={QuizPic} alt="quiz" />
+                </a>
             </div>
         </div>
 
